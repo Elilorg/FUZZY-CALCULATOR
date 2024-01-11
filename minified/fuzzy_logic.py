e797629e43a2c2783e8232dd7231ea37b4bb3855
Elilorg <elias.lorgnier@gmail.com>
2024-01-11 13:16:50 +0100

    vertion fonctionelle sur la numworks d'elias

--- minified/fuzzy_logic.py
+++ minified/fuzzy_logic.py
@@ -1,5 +1,19 @@
 _A=' '
 import math,random
+def Tp(a,b):return a*b
+def Tpp(a,b):
+	if b==1:return a
+	if a==1:return b
+	return 0
+def Tl(a,b):return max(0,a+b-1)
+Tnormes={'tp':Tp,'tl':Tl,'min':min,'tpp':Tpp}
+def cTp(a,b):return a+b-a*b
+def cTpg(a,b):
+	if a==0:return b
+	if b==0:return a
+	return 1
+def cTl(a,b):return min(1,a+b)
+Tconormes={'ctp':cTp,'ctl':cTl,'max':max,'ctpg':cTpg}
 class Erreur:
 	def __init__(B,message):A=message;print('Erreur log : '+A);B.message=A
 	def __str__(A):return'ERR: '+A.message
@@ -36,10 +50,12 @@ class Intervalle_net:
 class UnionIFT:
 	def __init__(A,IFTS,Tco=max):A.IFTS=IFTS;A.Tco=Tco
 	def possibilite(C,other):
-		B=other;A=0
+		B=other
 		if isinstance(B,UnionIFT):
+			A=0
 			for D in C.IFTS:A=max(A,D.possibilite(B))
 			return A
+		A=0
 		for E in C.IFTS:A=max(A,E.possibilite(B))
 		return A
 	def valeur(B,x):
@@ -50,6 +66,7 @@ class UnionIFT:
 		A=[A.alpha_coupe(alpha)for A in B.IFTS if A.alpha_coupe!=None];A=[(A.a1,A.a2)for A in A]
 		if len(A)>0:C,D=min([A[0]for A in A]),max([A[1]for A in A]);return Intervalle_net_continu(C,D)
 		else:return
+	def __str__(A):B=[B for B in Tconormes.keys()if Tconormes[B]==A.Tconorme][0];return B+' - Union de '+[str(A)for A in A.IFTS]
 class InterIFT:
 	def __init__(A,IFTS,Tno=min):A.IFTS=IFTS;A.Tno=Tno
 	def valeur(B,x):
@@ -60,6 +77,7 @@ class InterIFT:
 		A=[A.alpha_coupe(alpha)for A in B.IFTS if A.alpha_coupe!=None];A=[(A.a1,A.a2)for A in A]
 		if len(A)>0:C,D=max([A[0]for A in A]),min([A[1]for A in A]);return Intervalle_net_continu(C,D)
 		else:return
+	def __str__(A):B=[B for B in Tnormes.keys()if Tnormes[B]==A.Tnorme][0];return B+' - Intersection de '+[str(A)for A in A.IFTS]
 def line(p1,p2):B=p2;A=p1;C=A[1]-B[1];D=B[0]-A[0];E=A[0]*B[1]-B[0]*A[1];F,G=A;H,I=B;return C,D,-E,F,G,H,I
 def intersection(L1,L2):
 	B=L2;A=L1;C=A[0]*B[1]-A[1]*B[0];I=A[2]*B[1]-A[1]*B[2];J=A[0]*B[2]-A[2]*B[0];E,N,F,O=A[3:];G,P,H,Q=B[3:];K=max(min(E,F),min(G,H));L=min(max(E,F),max(G,H))
@@ -117,9 +135,9 @@ class Trapeseflou:
 	def valeur(A,x):
 		if x==A.a2:return A.h
 		if x<=A.a1:return 0
-		if A.a1<x<=A.a2:return A.h*((x-A.a1)/(A.a2-A.a1))
-		if A.a2<x<=A.a3:return A.h
-		if A.a3<x<=A.a4:return A.h*((A.a4-x)/(A.a4-A.a3))
+		if x>A.a1 and x<=A.a2:return A.h*((x-A.a1)/(A.a2-A.a1))
+		if x>A.a2 and x<=A.a3:return A.h
+		if x>A.a3 and x<=A.a4:return A.h*((A.a4-x)/(A.a4-A.a3))
 		if x>A.a4:return 0
 	def alpha_coupe(A,alpha):
 		B=alpha
@@ -138,37 +156,53 @@ def convert_to_float(nombre):
 	try:C=int(A)
 	except:return Erreur(str(A)+D)
 	return C
+class Scalaire(Trapeseflou):
+	def __init__(A,nb,h):super().__init__(nb,nb,nb,nb,h)
 def parseIFT(chaine):
-	A=[convert_to_float(A)for A in chaine.split(_A)if len(A)>0]
+	B='Bornes mal ordonnées';A=[convert_to_float(A)for A in chaine.split(_A)if len(A)>0]
 	if any(type(A)==Erreur for A in A):return A.index(key=lambda x:type(x)==Erreur)
-	B=len(A)
-	if any([C<A[B]for(B,C)in enumerate(A[1:])]):return Erreur('Bornes mal ordonnées')
-	match B:
-		case 2:return Intervalle_net_continu(A[0],A[1])
-		case 3:return Trapeseflou(A[0],A[1],A[1],A[2])
-		case 4:
-			if 0<A[3]<1:return Trapeseflou(A[0],A[1],A[1],A[2],h=A[3])
-			else:return Trapeseflou(A[0],A[1],A[2],A[3])
-		case 5:return Trapeseflou(A[0],A[1],A[2],A[3],h=A[4])
+	if len(A)==2:
+		if A[1]<=A[0]:return Erreur(B)
+		return Intervalle_net_continu(A[0],A[1])
+	if len(A)==3:
+		if not A[0]<=A[1]<=A[2]:return Erreur(B)
+		return Trapeseflou(A[0],A[1],A[1],A[2])
+	if len(A)==4:
+		if 0<A[3]<1:
+			if not A[0]<=A[1]<=A[2]:return Erreur(B)
+			return Trapeseflou(A[0],A[1],A[1],A[2],h=A[3])
+		else:
+			if not A[0]<=A[1]<=A[2]<=A[3]:return Erreur(B)
+			return Trapeseflou(A[0],A[1],A[2],A[3])
+	if len(A)==5:
+		if not A[0]<A[1]<A[2]<A[3]:return Erreur(B)
+		return Trapeseflou(A[0],A[1],A[2],A[3],h=A[4])
 	return Erreur('Pas le bon nombre de paramètres')
-def check_erreur(a,b):
-	if isinstance(a,Erreur):return a
-	elif isinstance(b,Erreur):return b
 def mul(a,b):
-	if(A:=check_erreur(a,b)):return A
-	return mul(a,b)
+	if type(a)==Erreur:return a
+	elif type(b)==Erreur:return b
+	return a.__mul__(b)
 def add(a,b):
-	if(A:=check_erreur(a,b)):return A
+	if type(a)==Erreur:return a
+	elif type(b)==Erreur:return b
 	return a+b
 def sub(a,b):
-	if(A:=check_erreur(a,b)):return A
+	if type(a)==Erreur:return a
+	elif type(b)==Erreur:return b
 	return a.__sub__(b)
 def div(a,b):
-	if(A:=check_erreur(a,b)):return A
+	if type(a)==Erreur:return a
+	elif type(b)==Erreur:return b
 	return a.__truediv__(b)
 def tronc(a,b):
-	if(A:=check_erreur(a,b)):return A
+	if type(a)==Erreur:return a
+	elif type(b)==Erreur:return b
 	return a.troncature(b)
+def pos(a,b):
+	if type(a)==Erreur:return a
+	elif type(b)==Erreur:return b
+	try:return a.possibilite(b)
+	except:return Erreur('Erreur dans la possibilité')
 def isnumeric(chaine):
 	try:float(chaine);return True
 	except:return False
